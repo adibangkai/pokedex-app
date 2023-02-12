0bf40c18f058e79ded22d96ed3636822d76ca36d
adi bangkit <adibangkai@gmail.com>
2023-02-12 19:31:14 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/pokedex-app/",
   plugins: [react()],
   root: "src",
-  server: {
-    port: 8000 || $PORT,
-  },
 });
